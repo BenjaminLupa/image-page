67b43bf1b9657528ec4eb8eaedb6a536dfb83737
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-03-27 11:28:52 +0100

    feat: added image-page

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <VApp>
+    <SideBar @select-option="onSelectOption" />
+    <VMain>
+      <ImageGallery :isWithColor="isWithColor" />
+    </VMain>
+  </VApp>
+</template>
+
+<script setup>
+import ImageGallery from "./components/ImageGallery.vue";
+import SideBar from "./components/SideBar.vue";
+import { ref } from "vue";
+
+const isWithColor = ref(true);
+
+const onSelectOption = (isColor) => {
+  isWithColor.value = isColor;
+};
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+        subtitle="bennylu1105@gmailcom"
+        title="Benjamin Lupa"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        @click="selectOption(true)"
+      />
+      <v-list-item
+        prepend-icon="mdi-dialpad"
+        title="Grey Gallery"
+        value="color"
+        @click="selectOption(false)"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script setup>
+import { defineEmits } from "vue";
+
+const emits = defineEmits(["selectOption"]);
+
+const selectOption = (isColor) => {
+  emits("selectOption", isColor);
+};
+</script>
